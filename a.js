97c451ea4671d4b852c26f51d07f41303a11b16e
Md-TarikulIslam <nayon1820@gmail.com>
2022-11-16 20:03:48 +0600

    first

---
+++ a.js
@@ -0,0 +1,19 @@
+const person = [
+    {
+        name: "rahim",
+        age: 22,
+        friends: ["rahim,karim,jabbar"],
+    },
+    {
+        name: "rahim2",
+        age: 22,
+        friends: ["rahim,karim,jabbar"],
+    },
+    {
+        name: "rahim3",
+        age: 22,
+        friends: ["rahim,karim,jabbar"],
+    },
+];
+
+console.log(person.slice(2, 1))
